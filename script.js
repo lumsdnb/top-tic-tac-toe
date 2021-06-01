94b52fad61c3f39230f2e33f08bcf88bfbd86573
lumsdnb <jeremy@lums.io>
2021-06-02 00:07:22 +0200

    Add player factory, minor cleanup

--- script.js
+++ script.js
@@ -4,38 +4,58 @@ boardEl.innerHTML=""
 
 let gameBoard = (function() {
   'use strict';
-
-  let _gameScore = [0,0,1,0,0,0,0,0,0];
-  var publicProperty = 'I am a public property';
-
-  function _renderDisplay() {
-    _gameScore.forEach(box=>{
-        const b = document.createElement("div")
-        b.innerHTML=box
-        b.classList.add("board-item")
-                b.classList.add("--hidden")
-        b.onclick= playerInput
-        boardEl.appendChild(b)
-    })
-    console.log(_gameScore);
-  }
-
-  function render() {
-    _renderDisplay();
-  }
   
-  function playerInput(e){
-      console.log(e.target);
-      if (![...e.target.classList].includes("selected")) {
-          e.target.classList.add("selected")
-          
-      } else console.log("already selected");
-  }
+  let _gameScore = [0,0,1,0,0,0,0,0,0];
+  
+  function _renderDisplay() {
+      _gameScore.forEach(box=>{
+          const b = document.createElement("div")
+          b.innerHTML=box
+          b.classList.add("board-item")
+          b.classList.add("--hidden")
+          b.onclick= playerInput
+          boardEl.appendChild(b)
+        })
+        console.log(_gameScore);
+    }
 
-  return {
-    render,
-    publicProperty: publicProperty
-  };
+    function getBoard(){
+        return _gameScore
+    }
+    
+    function render() {
+        _renderDisplay();
+    }
+    
+    function playerInput(e){
+        console.log(e.target);
+        if (![...e.target.classList].includes("selected")) {
+            e.target.classList.add("selected")
+            
+        } else console.log("already selected");
+    }
+    
+    return {render, getBoard};
 })();
 
 gameBoard.render()
+
+let playerFactory = (name) =>{
+    const sayHi=()=>{
+        console.log("hi")
+    };
+    return{sayHi, name}
+}
+
+const player1 = playerFactory("bob")
+
+const opponent=(name)=>{
+    const {sayHi} = playerFactory(name)
+    const playRound=()=>{
+        const boardState= gameBoard.getBoard()
+        console.log(boardState);
+    }
+    return{playRound,sayHi}
+}
+
+const dumbAI=opponent("jef")
