bf6229548598b9df475c09c40946d842938cc630
lumsdnb <jeremy@lums.io>
2021-06-01 12:51:27 +0200

    Add game board module w basic selection logic

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-let gameBoard = [0,0,1,0,0,0,0,0,0]
-const boardEl = document.querySelector(".gameboard")
+const boardEl = document.querySelector(".gameboard-grid")
 boardEl.innerHTML=""
-gameBoard.forEach(box=>{
-    const b = document.createElement("span")
-    b.innerHTML=box
-    b.classList.add("board-item")
 
-    boardEl.appendChild(b)
-})
+
+let gameBoard = (function() {
+  'use strict';
+
+  let _gameScore = [0,0,1,0,0,0,0,0,0];
+  var publicProperty = 'I am a public property';
+
+  function _renderDisplay() {
+    _gameScore.forEach(box=>{
+        const b = document.createElement("div")
+        b.innerHTML=box
+        b.classList.add("board-item")
+                b.classList.add("--hidden")
+        b.onclick= playerInput
+        boardEl.appendChild(b)
+    })
+    console.log(_gameScore);
+  }
+
+  function render() {
+    _renderDisplay();
+  }
+  
+  function playerInput(e){
+      console.log(e.target);
+      if (![...e.target.classList].includes("selected")) {
+          e.target.classList.add("selected")
+          
+      } else console.log("already selected");
+  }
+
+  return {
+    render,
+    publicProperty: publicProperty
+  };
+})();
+
+gameBoard.render()
